dc53157facb3c645aa148e41cffa597d2d42a8e5
SandraRvndrn <sandrarvndrn@gmail.com>
2023-10-13 10:33:05 +0530

    Add project files.

---
+++ CallbyReferece/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallbyReference
+{
+    public class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            abc a1, a2 = new abc(), a3 = new abc(), a4;
+
+            a2.i = 1;
+            a3.i = 2;
+            a1 = a3;
+            a4 = a2;
+            a1.i = 3;
+            a2.i = 4;
+            Console.WriteLine(a1.i);
+            Console.WriteLine(a2.i);
+            Console.WriteLine(a3.i);
+            Console.WriteLine(a4.i);
+
+            Console.ReadKey();
+        }
+    }
+    public class abc
+    {
+        public int i;
+    }
+}
